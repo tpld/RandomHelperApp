7ce7b1b12f739b2f9f43b7baeea620328c4975e0
4rko <artkopacz@gmail.com>
2015-03-08 12:05:29 +0100

    Encja User

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,142 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class User
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $email;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $password;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+    /**
+     * @ORM\Column(type="date")
+     */
+    protected $cratedAt;
+
+
+    public function __construct() {
+        $this->createdAt = new \DateTime("now");
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set cratedAt
+     *
+     * @param \DateTime $cratedAt
+     * @return User
+     */
+    public function setCratedAt($cratedAt)
+    {
+        $this->cratedAt = $cratedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get cratedAt
+     *
+     * @return \DateTime 
+     */
+    public function getCratedAt()
+    {
+        return $this->cratedAt;
+    }
+   
+}
